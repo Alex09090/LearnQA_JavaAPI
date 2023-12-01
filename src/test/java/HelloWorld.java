5d39239f44777733c84fd2a621e1d3f4be96b8b5
demoqa <sanews18@gmail.com>
2023-12-01 20:47:43 +0300

    Hello + name

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,22 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
 
 public class HelloWorld {
+
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello World!");
+        System.out.println("Hello, Александр");
+    }
+
+    @Test
+    public void testGetText() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 
 }
